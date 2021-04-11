7174e634daaa7e08de99e0e288b4cf7f754046cf
renukakasbe <79795601+renukakasbe@users.noreply.github.com>
2021-04-11 22:09:58 +0530

    Committing changes for index.html and other changes.

---
+++ learntech/language/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'C:\\Users\\vitth\\PycharmProjects\\untitled3\\learntech\\static\\index.html')
